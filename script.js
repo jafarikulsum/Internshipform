38cec94b052169d728185dfef84a5aa962d76925
jafarikulsum <163641470+jafarikulsum@users.noreply.github.com>
2025-01-25 21:34:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitForm() {
+    // Hide the form after submission
+    document.getElementById("internshipForm").style.display = "none";
+
+    // Show the success message
+    document.getElementById("successMessage").style.display = "block";
+}
